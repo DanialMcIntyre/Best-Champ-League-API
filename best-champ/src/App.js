e0f3400ee634707966ff98d8fae7a27712201a8d
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2022-12-28 17:20:41 -0500

    Can now get player data and show on screen

--- best-champ/src/App.js
+++ best-champ/src/App.js
@@ -5,12 +5,13 @@ import axios from 'axios';
 function App() {
 
   const API_KEY = "RGAPI-365230e6-285d-4a78-9278-7679e5a6c24e"
+  
+  var summonerName = "TheKartAgency";
+  const link = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + API_KEY;
 
   const [playerData, setPlayerData] = useState({})
 
   async function searchForPlayer() {
-    var summonerName = "TheKartAgency";
-    const link = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + API_KEY;
   
     axios.get(link).then(function (response) {
       setPlayerData(response.data)
@@ -22,12 +23,16 @@ function App() {
 
   searchForPlayer()
 
+  var id = playerData.id
+  var name = playerData.name
+
   return (
     
     <div className="App">
       <div className="content">
         <h1>Best Champion Finder!</h1>
-        <p></p>
+        <p>{id}</p>
+        <p>{name}</p>
       </div>
     </div>
   );
