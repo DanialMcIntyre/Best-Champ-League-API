fe30276b4f2fc1454c2f73f37d5e3a1aa43f7788
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2023-06-07 00:19:04 -0400

    Cleaned up and reformatted

--- best-champ/src/App.js
+++ best-champ/src/App.js
@@ -2,101 +2,49 @@ import './App.css';
 import {useState} from 'react'
 import axios from 'axios';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+import { useEffect } from 'react';
 
 function App() {
 
-  const API_KEY = "RGAPI-79c63bc8-e177-43bd-8f3f-ed32f47565c0"
+  //Text box
+  const [searchText, setSearchText] = useState("");
 
+  //Player data
   var [playerData, setPlayerData] = useState({})
-  var [matchHistoryData, setMatchHistoryData] = useState({})
+  //List of matches with details
+  var [matchData, setMatchData] = useState({})
 
-  //Gets player data from API
-  async function searchForPlayer(name) {
-    const link = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + API_KEY;
+  //When user searches for player
+  function onButtonClick(event) {
+    getPlayerData()
+    getMatchData()
+  }
 
-    //Gets info from API
-    axios.get(link).then(function (response) {
-      setPlayerData(response.data)
-      //When gets player data, also get match history
-      searchForMatches(response.data.puuid)
+  //Finds player data
+  function getPlayerData() {
+    axios.get("http://localhost:4000/playerData", { params: {username: searchText}})
+    .then(function (response) {
+      setPlayerData(response.data);
     }).catch(function (error) {
       console.log(error);
     })
   }
 
-  //Gets match history of player from API
-  async function searchForMatches(puuid) {
-    const link = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=100&api_key=" + API_KEY;
-
-    //Gets info from API
-    axios.get(link).then(function (response) {
-      setMatchHistoryData(response.data)
-      matchHistoryData = Object.values(matchHistoryData);
-    }).catch(function (error) {
-      console.log(error);
-    })
-  }
-  //
-  //Gets specific match information at certain index, returns JSON
-  function getSpecificMatch(matches, index) {
-    return new Promise((resolve, reject) => {
-      const link = "https://americas.api.riotgames.com/lol/match/v5/matches/" + matches[index] + "?api_key=" + API_KEY;
-      axios.get(link).then(function (response) {
-        let matchInfo = JSON.stringify(response.data);
-          let result = getSpecificMatchData(matchInfo);
-          setTimeout(() => {
-            resolve(result)
-            }, 1)
+  //Gets match details of past games
+  function getMatchData() {
+    axios.get("http://localhost:4000/matchHistory", { params: {username: searchText}})
+      .then(function (response) {
+        setMatchData(response.data);
       }).catch(function (error) {
         console.log(error);
       })
-    })
-  }
-
-  function getSpecificMatchData(match) {
-    //Puts match info in variable
-    var data = JSON.parse(match);
-    let participants = data.metadata.participants;
-    let yourPlayerIndex = 0;
-
-    //Gets all info for player
-    for (let i = 0; i < participants.length; i++) {
-      if (participants[i] === playerData.puuid) {
-        yourPlayerIndex = i;
-      }
     }
-    let yourPlayer = data.info.participants[yourPlayerIndex];
 
-    let matchchampion = yourPlayer.championName;
-    let matchlane = yourPlayer.lane;
-    let matchkills = yourPlayer.kills;
-    let matchdeaths = yourPlayer.deaths;
-    let matchassists = yourPlayer.assists;
-    let thismatch = {champion : matchchampion, lane : matchlane, kills : matchkills, deaths : matchdeaths, assists : matchassists}
-    return thismatch
-  }
-  
-  async function getAllMatches() {
-    const matchArr = [];
-    for(let i = 0; i < 20; i++) {
-      matchArr.push(await getSpecificMatch(matchHistoryData, i));
-    }
-    return matchArr
-  }
-  async function receiveData() {
-      const matches = await getAllMatches();
-      console.log(matches)
-      //let imgString = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"
-      //imgString = imgString + match1.champion + "_0.jpg"
-      //document.getElementById("champImg").setAttribute("src", imgString)
-      return //match1.champion
-  }
+  useEffect(() => {
+    console.log(playerData);
+    console.log(matchData);
+    }, [playerData, matchData]); 
 
-  receiveData();
   //Puts info into variables
   var playerID = playerData.id
   var playerPUUID = playerData.puuid
@@ -104,79 +52,26 @@ function App() {
   let imgString = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"
   imgString = imgString + "Aatrox_0.jpg"
   var bestChamp = "Thresh"
-  
-  function MyForm() {
-    const [name, setName] = useState("");
-  
-    //When form is submitted
-    const handleSubmit = (event) => {
-      event.preventDefault();
-
-      //Reset player data
-      playerData = {}
-      
-      //Continues checking API as long as object doesn't have any info
-      if (Object.keys(playerData).length === 0) {
-        searchForPlayer(name)
-      }
-
-    }
-  
-    return (
-      <Box 
-          component="form"
-          sx={{
-            position: "relative",
-          }}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit}
-        > 
-          <Box class="searchbox-container">
-            <Box class="searchbox-spacer"></Box>
-            <TextField id="outlined-basic" label="Summoner Name" variant="standard" type="text" value={name} onChange={(e) => setName(e.target.value)}
-              sx={{
-                width: "85%",
-                height: "10%"
-              }}
-            />
-            <IconButton color="primary" aria-label="upload picture" component="label" onClick={handleSubmit}
-              sx={{
-                width: "5%",
-                height: "10%"
-              }}
-            
-            >
-              <SearchIcon></SearchIcon>            
-            </IconButton>
-            <Box class="searchbox-spacer"></Box>
-
-            
-          </Box>
-        </Box>
-    )
-  }
 
   function Analytics() {
 
     return (
-      <div class="flex-container">
-        <div class="spacer"></div>
-        <div class="child1"> 
-          <MyForm/>
-          <p display="block" class="analytics">{playerName}</p> 
+      <div className="flex-container">
+        <div className="spacer"></div>
+        <div className="child1"> 
+          <p display="block" className="analytics">{playerName}</p> 
           <img width="100%" src={imgString} alt="Your best champion" id="champImg" display="inline"/>
         </div>
         <div id="midspacer"></div>
-        <div class="child2">
+        <div className="child2">
           <h1 id="title">Your Best Champ is: {bestChamp} </h1>
-          <div class="analytics"> 
-            <p display="block" class="analytics">Player ID: {playerID}</p>
-            <p display="block" class="analytics">Player Puuid: {playerPUUID}</p>
-            
+          <div className="analytics"> 
+            <p display="block" className="analytics">Player ID: {playerID}</p>
+            <p display="block" className="analytics">Player Puuid: {playerPUUID}</p>
+
           </div>
         </div>
-        <div class="spacer"></div>
+        <div className="spacer"></div>
       </div>
     )
   }
@@ -186,11 +81,10 @@ function App() {
     <div className="App">
       <div className="content">
         <h1>Best Champion Finder!</h1>
-            
-        
+        <input type="text" onChange = {e => setSearchText(e.target.value)}/>
+        <button onClick = {e => onButtonClick(e)}>Search for player</button>
+        <Analytics/>
         <br />
-        <Analytics />
-        
         
       </div>
     </div>
---
+++ proxy-server/proxyServer.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var cors = require('cors');
+const axios = require('axios');
+
+var app = express();
+
+app.use(cors());
+
+const API_KEY = "RGAPI-490081c2-81c7-4ba7-a06a-73196dbccbe9";
+
+//Gets player puuid
+function getPlayerPUUID(playerName) {
+
+    const link = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + playerName + "?api_key=" + API_KEY;  
+    return axios.get(link)
+        .then(response => {
+            return response.data.puuid;
+        }).catch(err => err);
+}
+
+//Get player data
+app.get('/playerData', async (req, res) => {
+    const searchName = req.query.username;
+    const link = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchName + "?api_key=" + API_KEY;  
+    const playerData = await axios.get(link)
+        .then(response => response.data)
+        .catch(err => err);
+    
+    res.json(playerData);
+})
+
+//Get list of matches and details
+app.get('/matchHistory', async (req, res) => {
+    const puuid = await getPlayerPUUID(req.query.username);
+    const numberOfGames = 5;
+    const link1 = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=" + numberOfGames + "&api_key=" + API_KEY;
+    const matchIDList = await axios.get(link1)
+        .then(response => response.data)
+        .catch(err => err);
+
+    let matchListDetails = []
+    for (let i = 0; i < matchIDList.length; i++) {
+        const link2 = "https://americas.api.riotgames.com/lol/match/v5/matches/" + matchIDList[i] + "?api_key=" + API_KEY;
+        const details = await axios.get(link2)
+            .then(response => response.data)
+            .catch(err => err);
+            matchListDetails.push(details);
+    }
+
+    res.json(matchListDetails);
+});
+
+app.listen(4000, function () {
+    console.log("Server started on port 4000");
+});
